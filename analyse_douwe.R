ddd02cc84097190a13daaa62dd0f5e1d19498bf4
Douwe Metselaar <dgmetselaar@bioinf.nl>
2021-06-03 16:20:43 +0200

    labjournaal bewerkt

--- analyse_douwe.R
+++ analyse_douwe.R
@@ -21,3 +21,12 @@ featureCounts_output <- featureCounts(files = bam_files,
 count_data <- as.data.frame(featureCounts_output$counts)
 colnames(count_data) <- c("root_treatment_1", "root_treatment_2", "root_treatment_3", 
                           "root_control_1", "root_control_2", "root_control_3")
+
+#Save the count data as a csv file
+write.csv(count_data, "data/root_counts.csv")
+
+#----------------------------------------------------------------------
+#Load the root count data
+root_counts <- read.table("data/root_counts.csv", header = T, sep = ",", row.names = 1)
+
+
